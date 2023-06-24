d2289a04fbad57e7b4caeda862532e06ac991543
Denis K <destroymarsk@gmail.com>
2023-06-24 10:42:52 +0300

    feat: Added button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import cn from "classnames";
+
+import styles from "./Button.module.css";
+
+type ButtonVariants = "outline" | "icon";
+type ButtonSizes = "default" | "small";
+
+interface Props extends React.ComponentPropsWithoutRef<"button"> {
+  variant?: ButtonVariants;
+  size?: ButtonSizes;
+  children: React.ReactNode;
+}
+
+const buttonVariantsClasses: Record<ButtonVariants, string> = {
+  outline: styles.button_outline,
+  icon: styles.button_icon,
+};
+
+const buttonSizesClasses: Record<ButtonSizes, string> = {
+  default: styles.button_default_size,
+  small: styles.button_small_size,
+};
+
+export default function Button({
+  children,
+  className,
+  variant = "default",
+  size = "default",
+  disabled,
+  ...rest
+}: Props) {
+  return (
+    <button
+      className={cn(
+        styles.button,
+        buttonVariantsClasses[variant],
+        buttonSizesClasses[size],
+        { [styles.button_disabled]: disabled },
+        className
+      )}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
+}
