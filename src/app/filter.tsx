ed85a27a9fd43524e171c490b061db5238f13589
Denis K <destroymarsk@gmail.com>
2023-06-26 17:47:17 +0300

    feat: Added debounce to the filter

--- src/app/filter.tsx
+++ src/app/filter.tsx
@@ -7,18 +7,24 @@ import styles from "./filter.module.css";
 import textVariants from "@/styles/textVariants.module.css";
 import Dropdown from "@/components/Dropdown/Dropdown";
 import BoxWrapper from "@/components/Box/Box";
-import { useGetCinemaQuery } from "@/store/features/cart/filmsApi";
 import { Cinema } from "@/models/Cinema";
 import { useDispatch, useSelector } from "react-redux";
-import { changeCinemaFilter, type CartState, CartSlice, changeNameFilter, changeGenreFilter } from "@/store/features/cart/cartSlice";
+import { changeCinemaFilter, type CartState, CartSlice, changeGenreFilter, changeNameFilter } from "@/store/features/cart/cartSlice";
 import { FilmGenre, GenreLocalization } from "@/models/Film";
 import { DropdownItem } from "@/components/Dropdown/Dropdown";
-import { useMemo } from "react";
+import {  useMemo, useState } from "react";
+import { useDebouncedEffect } from "@/hooks/useDebounceEffect";
 
 export default function Filter({cinemas} : {cinemas?: Cinema[]}) {
   const filter = useSelector<CartSlice, CartState["filter"]>(({ cart }) => cart.filter)
 
   const dispatch = useDispatch()
+
+  const [nameFilter, setNameFilter] = useState(filter.name)
+
+  useDebouncedEffect(() => {
+    dispatch(changeNameFilter(nameFilter))
+  }, [nameFilter])
 
   const cinemasDropdown: DropdownItem[] = useMemo(() => {
     return cinemas ? cinemas.map(({id, name}): DropdownItem => ({label: name, value: id})) : []
@@ -38,8 +44,8 @@ export default function Filter({cinemas} : {cinemas?: Cinema[]}) {
       </h3>
       <form className={styles.filterGroup}>
         <Input 
-        placeholder="Введите название" label="Название" value={filter.name ?? ''} 
-        onInput={(e) => dispatch(changeNameFilter((e.target as HTMLInputElement).value))}
+          placeholder="Введите название" label="Название" value={nameFilter ?? ""} 
+          onInput={(e) => setNameFilter((e.target as HTMLInputElement).value)}
         />
         <Dropdown
         selectedValue={filter.genre}
