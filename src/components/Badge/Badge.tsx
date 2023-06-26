6af502a541d8a9def736ed6e63ba26ef4f54a09e
Denis K <destroymarsk@gmail.com>
2023-06-26 13:21:40 +0300

    feat: Added badge component

---
+++ src/components/Badge/Badge.tsx
@@ -0,0 +1,9 @@
+import styles from './Badge.module.css'
+
+export default function Badge({value} : {value: string | number}) {
+    return (
+        <div className={styles.badge}>
+            <span className={styles.badge__value}>{value}</span>
+        </div>
+    )
+}
